49025794c0f7108f8c0ed870d87ab7bf9daa8474
Michon van Dooren <michon1992@gmail.com>
2017-07-09 20:22:07 +0200

    Made a very basic add entry screen using react-native-elements

--- src/screens/AddEntry.js
+++ src/screens/AddEntry.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements';
 import withNavigation from '../components/Navigation';
 
 const styles = StyleSheet.create({
+	container: {
+	},
 });
 
 class AddEntry extends React.Component {
 	render() {
 		return (
-			<View>
-				<Text>Hello world!</Text>
+			<View style={styles.container}>
+				<FormLabel>Username</FormLabel>
+				<FormInput onChangeText={this.onChangeName} placeholder="Name" />
+
+				<FormLabel>URL</FormLabel>
+				<FormInput onChangeText={this.onChangeURL} placeholder="URL" />
+
+				<FormLabel>Secret</FormLabel>
+				<FormInput onChangeText={this.onChangeSecret} placeholder="Secret" />
 			</View>
 		);
 	}
