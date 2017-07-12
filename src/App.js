c96e4efd2d913b3596c4a32d7c04fb194f47307c
Michon van Dooren <michon1992@gmail.com>
2017-07-12 14:25:13 +0200

    Only load the redux devtools in development

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, autoRehydrate, toReduxPersistStorage } from './utils/persist';
 import * as storage from './utils/storage';
 import rootReducer from './reducers';
 import Home from './screens/Home';
 import AddEntry from './screens/AddEntry';
 
+const compose = process.env.NODE_ENV === 'development'
+	? require('redux-devtools-extension').composeWithDevTools // eslint-disable-line import/no-extraneous-dependencies
+	: require('redux').compose;
+
 // Setup the Redux store
 const store = createStore(
 	rootReducer,
 	undefined,
-	composeWithDevTools(
+	compose(
 		applyMiddleware(),
 		autoRehydrate(),
 	),
