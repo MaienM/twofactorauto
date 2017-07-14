46c08698b157922151d8a3a134251508f39f1990
Michon van Dooren <michon1992@gmail.com>
2017-07-14 16:17:55 +0200

    Extracted the extracting of the route params from the props to a utility function

--- src/screens/EditEntry.js
+++ src/screens/EditEntry.js
@@ -1,19 +1,18 @@
 import { connect } from 'react-redux';
 import { updateEntry } from '../actions/entries';
+import { getParams } from '../utils/navigation';
 import EntryForm from './EntryForm';
 
-const getUUID = (ownProps) => ownProps.navigation.state.params.uuid;
-
 const mapStateToProps = (state, ownProps) => ({
-	entry: state.entries[getUUID(ownProps)],
-	secrets: state.secrets[getUUID(ownProps)],
+	entry: state.entries[getParams(ownProps).uuid],
+	secrets: state.secrets[getParams(ownProps).uuid],
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
 	onSave: ({ entry, secrets }) => {
 		ownProps.navigation.goBack();
 		dispatch(updateEntry({
-			uuid: getUUID(ownProps),
+			uuid: getParams(ownProps).uuid,
 			entry,
 			secrets,
 		}));
