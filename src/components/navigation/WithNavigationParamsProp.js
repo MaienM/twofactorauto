435a62f32ab16acd7e986c8b3c7d9bbf5e14aeeb
Michon van Dooren <michon1992@gmail.com>
2017-07-22 22:49:31 +0200

    Added a HOC that transforms react-navigation's params to a prop

---
+++ src/components/navigation/WithNavigationParamsProp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import setupHOC from '../../utils/hoc';
+
+/**
+ * A higher-order component that passes the navigation params of the current screen as a prop to the component.
+ */
+export default (Component) => {
+	const withNavigationParamsProp = (props) => <Component {...props} />;
+	setupHOC(Component, withNavigationParamsProp);
+
+	const mapStateToProps = (state) => ({
+		params: state.navigation.navigator.routes[state.navigation.navigator.index].params,
+	});
+
+	return connect(mapStateToProps)(withNavigationParamsProp);
+};
+
