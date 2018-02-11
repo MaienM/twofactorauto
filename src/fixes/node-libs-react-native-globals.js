2988e8f1033a0d5d52bb4b33c48fbf2f70b43f8e
Michon van Dooren <michon1992@gmail.com>
2018-02-11 22:31:38 +0000

    Added a check to node-list-react-native-globals to prevent it from taking effect in debug mode, where it breaks

--- src/fixes/node-libs-react-native-globals.js
+++ src/fixes/node-libs-react-native-globals.js
@@ -5,12 +5,14 @@
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
 
-// Needed so that 'stream-http' chooses the right default protocol.
-global.location = {
-  protocol: 'file:',
-};
+if (!__DEV__) {
+	// Needed so that 'stream-http' chooses the right default protocol.
+	global.location = {
+	  protocol: 'file:',
+	};
 
-// Some modules expect userAgent to be a string
-global.navigator.userAgent = 'React Native';
+	// Some modules expect userAgent to be a string
+	global.navigator.userAgent = 'React Native';
+}
 
 /* eslint-enable */
